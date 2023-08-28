9014da257684ca5586295ca3218b7c7813bfba11
ZainabKhalid21 <zainabkhalid.intern@devsinc.com>
2023-08-28 15:43:46 +0500

    Fixed errors

--- bugzilla/bugzilla/settings.py
+++ bugzilla/bugzilla/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import environ
 
 env = environ.Env()
@@ -139,9 +140,8 @@ LOGIN_REDIRECT_URL = 'homepage'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com' 
-EMAIL_PORT = 587  
-EMAIL_USE_TLS = True  
-EMAIL_USE_SSL = False 
-EMAIL_HOST_USER = 'zainabkhalid.intern@devsinc.com'  
-EMAIL_HOST_PASSWORD = 'blqerjlfrtldnscn'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
